0bfd11be1a7d5f00181a13feba7f05e6e163f92c
Kajetan Stepien <grizzliesrecords@gmail.com>
2024-12-11 19:58:51 +0100

    added script files, designed prototype blackjack settings menu,added first functionalities like opening settings menu after choosing blackjack game/closing it

---
+++ dist/ui.js
@@ -0,0 +1,36 @@
+const bjOption = document.getElementById("bj-btn");
+const texasOption = document.getElementById("texas-btn");
+const bjMenu = document.getElementById("bj-menu");
+const texasMenu = document.getElementById("texas-menu");
+function hideOptions(menuToShow) {
+    bjOption.classList.add("hidden");
+    texasOption.classList.add("hidden");
+    menuToShow.classList.remove("hidden");
+}
+function showOptions(menuToHide) {
+    bjOption.classList.remove("hidden");
+    texasOption.classList.remove("hidden");
+    menuToHide.classList.add("hidden");
+}
+if (bjOption) {
+    bjOption.addEventListener("click", () => {
+        hideOptions(bjMenu);
+        const backBtn = document.getElementById("exit-btn");
+        if (backBtn) {
+            backBtn.addEventListener("click", () => {
+                showOptions(bjMenu);
+            });
+        }
+    });
+}
+else {
+    console.log("Element bj-btn does not exist.");
+}
+if (texasOption) {
+    texasOption.addEventListener("click", () => {
+        hideOptions(texasMenu);
+    });
+}
+else {
+    console.log("Element texas-btn does not exist.");
+}
---
+++ script/ui.ts
@@ -0,0 +1,37 @@
+const bjOption: HTMLElement | null = document.getElementById("bj-btn");
+const texasOption: HTMLElement | null = document.getElementById("texas-btn");
+const bjMenu: HTMLElement | null = document.getElementById("bj-menu");
+const texasMenu: HTMLElement | null = document.getElementById("texas-menu");
+
+function hideOptions(menuToShow: HTMLElement) :void {
+    bjOption.classList.add("hidden");
+    texasOption.classList.add("hidden");
+    menuToShow.classList.remove("hidden");
+}
+function showOptions(menuToHide: HTMLElement) :void {
+    bjOption.classList.remove("hidden");
+    texasOption.classList.remove("hidden");
+    menuToHide.classList.add("hidden");
+}
+
+if(bjOption){
+    bjOption.addEventListener("click", () => {
+        hideOptions(bjMenu);
+        const backBtn = document.getElementById("exit-btn") as HTMLButtonElement;
+        if(backBtn){
+            backBtn.addEventListener("click", () => {
+                showOptions(bjMenu);
+            })
+        }
+    })
+}else{
+    console.log("Element bj-btn does not exist.");
+}
+
+if(texasOption){
+    texasOption.addEventListener("click", () => {
+        hideOptions(texasMenu);
+    })
+}else{
+    console.log("Element texas-btn does not exist.");
+}
